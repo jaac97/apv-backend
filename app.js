4ef9fc17f841ca986aa155e8ed1fa0d843955f7a
Josué Alarcón <josuealarco@gmail.com>
2022-08-11 09:28:33 -0500

    Add whitelist

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ console.log(whitelist)
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 } */
 // Use Cors
-app.use(cors())
+app.use(cors(whitelist))
 
 
 

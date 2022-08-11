e5f7d9cd7292cb5e4fd4598bf083aae77460f6bf
Josué Alarcón <josuealarco@gmail.com>
2022-08-11 09:41:27 -0500

    cors empty

--- app.js
+++ app.js
@@ -37,9 +37,7 @@ console.log(whitelist)
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 } 
 // Use Cors
-app.use(cors(corsOptions))
-
-
+app.use(cors())
 
 
 // Define Routes
